e7ec206aded412cf128e10111406b1ac021ce00a
Sarah Ribeiro de Souza <sarahrsz162@gmail.com>
2026-06-18 14:23:09 -0300

    chore: estrutura inicial do projeto

---
+++ Backend/busalert/src/main/java/com/fatec/busalert/BusalertApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.busalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusalertApplication.class, args);
+	}
+
+}
---
+++ Backend/busalert/src/test/java/com/fatec/busalert/BusalertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.busalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
